db08dcf70ee0327ab12f2265b7342e5054d04119
Dillon Drenzek <dr.enzek@gmail.com>
2015-02-20 19:11:28 -0700

    new cards rendering

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.index);
-
 app.get('/skills', routes.skills);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening at: http://localhost:%d/', app.get('port'));
