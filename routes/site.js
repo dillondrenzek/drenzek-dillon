f962397555e7fea68ae191f23efe1ec703ca20eb
Dillon Drenzek <dr.enzek@gmail.com>
2015-03-01 11:58:59 -0700

    beginning final push for v1.0

--- routes/site.js
+++ routes/site.js
@@ -1,7 +1,22 @@
 var site = exports = module.exports;
 
+var Project = require('../models/project'),
+	Skill = require('../models/skill');
+
 
 // GET '/'
 site.index = function(req, res, next) {
-	res.render('index');
+
+	Project.getAll(function (err, p_results) {
+        if (err) return next(err);
+
+        Skill.getAll(function (err, s_results) {
+        	if (err) return next(err);
+
+        	res.render('index', {
+				projects: p_results,
+				skills: s_results
+			});
+        });
+    });
 };
