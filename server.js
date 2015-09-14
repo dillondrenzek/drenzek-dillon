ba7fefb3fc52b9b882ff9b88a21ae7ab46fbd1ca
Dillon Drenzek <dr.enzek@gmail.com>
2015-09-14 13:24:02 -0600

    removed index.js, is now server.js

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
+// Server
+// version 2.2.0
+
+var exports = module.exports = {};
+
+// Module Dependencies
+var express = require('express')
+  , routes = require('./routes')
+  , path = require('path');
+
+// Create and Configure App
+var app = exports.app = express();
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+
+
+
+// App Routes
+app.get('/', routes.index);
+
+
+
+// Create Server
+var server = app.listen(app.get('port'), app.get('ip'), function(){
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Express server listening at http://%s:%s', host, port);
+});
+
+
 // #!/bin/env node
 // //  OpenShift sample Node application
 // var express = require('express');
