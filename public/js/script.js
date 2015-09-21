516866e6ce3a3a0f544c087d81875bf79eba1f1b
Dillon Drenzek <dr.enzek@gmail.com>
2015-09-21 14:30:26 -0600

    .project figure now sizing correctly, modal still a little shaky

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,21 @@
 (function($, window){
 	$(function(){
 
+		function sizeImage(img){
+			var $img = (img instanceof jQuery) ? img : $(img);
+			var frame = $img.closest('figure');
+			var frameRatio = $(frame).width()/$(frame).height();
+			console.log(frameRatio);
+
+			if ($img.width()/$img.height() > frameRatio) {
+				$img.addClass('landscape');
+			} else {
+				$img.addClass('portrait');
+			}
+		};
+
 		function initializeModal(){
+
 			$('.close')
 				.click(function(e){
 					e.preventDefault();
@@ -19,21 +33,28 @@
 						console.log("right");
 					} });
 
+			sizeImage($('#modal').find('img'));
+
 			// Hide Modal
 			$('#modal').hide();
 		};
 
 		function presentModal(slider){
 			var $slider = $(slider);
-			var images = $slider.find('img');
+			var images = $slider
+				.find('img')
 			$('#modal')
-				.find('img.modal')
-				.attr({"src": $(images[0]).attr("src")});
+				.find('img')
+				.attr({"src": $(images[0]).attr("src")})
+				.each(function(){
+					sizeImage($(this));
+				});
+
+			
 
 			$('#modal').show();
 
 		};
-
 		
 
 		// Initialize Project Figures
@@ -43,7 +64,12 @@
 								"class" : "image",
 								"text" : "Click to View More Photos"}))
 			.each(function(){
-				$(this).height($(this).width() / frameRatio); })
+				// Size figure
+				$(this).height($(this).width() / frameRatio);
+
+				// size image
+				sizeImage($(this).find('img')[0]); 
+			})
 			.click(function(e){
 				e.preventDefault();
 				presentModal(this); 
