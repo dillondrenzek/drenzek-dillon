4138233f77c27901b13fd1facdabc6975fd29a1a
Dillon Drenzek <dr.enzek@gmail.com>
2015-09-20 23:06:48 -0600

    rearranging stylus sheets

--- public/js/script.js
+++ public/js/script.js
@@ -1,52 +1,18 @@
 (function($){
 	$(function(){
 
-		// $this.images = $this.find('img');
-		// $this.images.each(function(i,e,a){
-		// 	var $e = $(e).remove();
-		// 	var $slide = $("<div>", {
-		// 		class: "slide"
-		// 	});
-		// 	$slide.append($e);
-		// 	$this.tray.append($slide);
-
-		// 	// $this.slides.push($slide);
-
-		
-		// });
-		// $this.slides = $this.find('.slide');
-
 
 
 
 
 		function presentSlider(slider){
 			var $slider = $(slider);
-			// console.log("slider clicked", slider);
 			$('#modal').show();
-
-			// $slider
-
-			// $slider
-			// 	.find('img')
-			// 	.each(function(i, e){
-			// 		var $e = $(e).remove();
-			// 		var $slide = $("<div>", { class: "slide" })
-			// 			.append($e);
-			// 		if ($e.width() / $e.height() > frameRatio) {
-			// 			$e.addClass("landscape");
-			// 		} else {
-			// 			$e.addClass("portrait");
-			// 		};
-			// 	})
-
 		};
 
 		$('#modal').hide();
 
 		var frameRatio = 4/3;
-		// var frameWidth = $this.width();
-		// var frameHeight = $this.height(frameWidth / frameRatio);
 
 		$('.image-slider')
 			.prepend($("<a>", { "href" : "#",
@@ -59,13 +25,20 @@
 				presentSlider(this); 
 			});
 
-		// $('a.image')
-		// 	;
-
 		$('.close')
 			.click(function(e){
 				e.preventDefault();
 				$('#modal').hide(); });
+
+		$('.arrow')
+			.click(function(e){
+				e.preventDefault();
+				if ($(this).hasClass("left")) {
+					console.log("left");
+				} else if ($(this).hasClass("right")) {
+					console.log("right");
+				}
+			})
 			
 	});
 })(jQuery);
