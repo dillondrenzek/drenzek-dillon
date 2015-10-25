cbb3801a2f748bf76a4813a156dd3d71a02874b1
Dillon Drenzek <dr.enzek@gmail.com>
2015-10-24 20:31:42 -0600

    projects display correctly as well

--- models/Project.js
+++ models/Project.js
@@ -2,7 +2,7 @@ var Project = module.exports;
 var MongoClient = require('mongodb').MongoClient;
 
 Project.getAll = function(callback){
-	MongoClient.connect("localhost:27017/drenzek-dillon", function(err, db){
+	MongoClient.connect("mongodb://localhost:27017/drenzek-dillon", function(err, db){
 		if (err) throw err;
 		
 		db.collection("projects").find({}).toArray(function(err, docs){
