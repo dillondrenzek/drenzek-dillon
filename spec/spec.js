ec199999e758adf6e07ba08e7fe80ef6e2b92db7
Dillon Drenzek <dr.enzek@gmail.com>
2015-10-25 22:26:34 -0600

    tests running

--- spec/spec.js
+++ spec/spec.js
@@ -1,10 +1,30 @@
 // spec.js
 // v3.0.4
 
-describe('Test', function(){
-	it('should have a title', function(){
-		browser.get('http://localhost:8080/the-work-of');
+describe('Admin Home', function(){
 
-		expect(browser.getTitle()).toEqual('Dillon Drenzek, The Work Of');
+	beforeEach(function(){
+		browser.get('http://localhost:8080/the-work-of/admin');
+	});
+
+	// describe('Protractor Demo App', function() {
+	//   it('should have a title', function() {
+	//     browser.get('http://juliemr.github.io/protractor-demo/');
+
+	//     // expect(browser.getTitle()).toEqual('Super Calculator');
+
+	//   });
+	// });
+
+	it('should have a page title', function(){
+		
+		var pageTitle = element(by.binding('pageTitle'));
+		expect(pageTitle.getText()).toEqual('Admin Home');
+
+
+		// element(by.binding('pageTitle')).then(function(pageTitle){
+		// 	expect(pageTitle.getText()).toEqual('Admin Home');
+		// });
+		
 	});
 });
